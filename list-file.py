337d45e30a26c68a9ba40e414f41b8a8dbf3679f
Alsadig-ahmed <alsadq.ahmd@gmail.com>
2025-04-17 19:37:15 +0200

    list and delete files uploaded to gemini

---
+++ list-file.py
@@ -0,0 +1,17 @@
+from google import genai
+import os 
+
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+
+# list using the File API
+for file in client.files.list():
+    print(file.name)
+
+# delete a file 
+response = client.files.delete(name="files/7mlwugy8icld")
+
+print("\n\n")
+for file in client.files.list():
+    print(file.name)
+# file = client.files.get(name=file.name)
+# print(file)
